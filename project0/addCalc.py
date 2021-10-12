28c0958b5fdae1cb95a711b92972bfd0c6a6b1ac
Marco Troper <marcotroper55@gmail.com>
2021-10-12 11:07:54 -0700

    Updated 3rd commit

--- project0/addCalc.py
+++ project0/addCalc.py
@@ -1,5 +1,5 @@
 def calc(num1,num2):
-    return (num1+num2)
-print(calc(2,3))
-print(calc(7,19))
-print(calc(-4,1))
+    print("Number 1: " + str(num1) + " Number 2: " + str(num2) + " Sum: " + str((num1+num2)))
+calc(2,3)
+calc(7,19)
+calc(-4,1)
