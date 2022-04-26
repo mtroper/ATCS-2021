e0a4537872bd6610c76cc77bb85cb08c5b2b8b5e
Marco Troper <marcotroper55@gmail.com>
2022-04-26 13:50:39 -0700

    first commit

--- MachineLearning/code.py
+++ MachineLearning/code.py
@@ -1,6 +1,14 @@
-import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import PIL
 import tensorflow as tf
+
+
+import tensorflow.contrib.keras as keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+data_dir = "./ocr_training_data"
+
+print(len(data_dir))
