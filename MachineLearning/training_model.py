f6b499121b080f726cf62254350daf1607b8bf68
Marco Troper <marcotroper55@gmail.com>
2022-05-02 09:57:34 -0700

    Complete training model for ocr

--- MachineLearning/training_model.py
+++ MachineLearning/training_model.py
@@ -6,11 +6,73 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
-
+training_data = {}
 dir = "ocr_training_data/"
 for folder in os.listdir(dir):
     if folder != ".DS_Store":
         dir += (folder + "/")
+        list = []
         for images in os.listdir(dir):
-            print(folder,images)
+            list.append(str(dir + images))
+        training_data.update({folder:list})
     dir = "ocr_training_data/"
+
+##############################################
+batch_size = 32
+img_height = 180
+img_width = 180
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+"ocr_training_data/",
+validation_split=0.01,
+subset="training",
+seed=123,
+image_size=(img_height, img_width),
+batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+"ocr_training_data/",
+validation_split=0.01,
+subset="validation",
+seed=123,
+image_size=(img_height, img_width),
+batch_size=batch_size)
+
+class_names = train_ds.class_names
+print(class_names)
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+normalization_layer = layers.Rescaling(1./255)
+
+num_classes = len(class_names)
+
+model = Sequential([
+  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+epochs=15
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+
+
+
